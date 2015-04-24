968a31b38b8e4115d580f1b902679502bee1f96d
orangeoval <anorangeoval@gmail.com>
2015-04-24 08:22:10 -0400

    added template paths to settings, and created initial overview.html view

--- scotus/urls.py
+++ scotus/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'scotus.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'scotus.views.overview', name='overview'),
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ scotus/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def overview(request):
+    return render(request, 'overview.html')
