235c9112437cb35d389ceb7b9cefcb6cffe82ad8
orangeoval <anorangeoval@gmail.com>
2015-04-25 13:42:21 -0400

    reaplced HTTPParser structure wil lxml in Discovery app

--- discovery/models.py
+++ discovery/models.py
@@ -1,53 +1,10 @@
+from scotus import settings
 from django.db import models
 from opinions.models import Opinion
-from HTMLParser import HTMLParser
+from citations.models import Citation
 from requests import get
-
-class SCOTUSParser(HTMLParser):
-    def __init__(self):
-        self.in_pannel = False
-        self.in_opinion_table = False
-        self.in_table_row = False
-        self.table_data = []
-        self.row_data = []
-        self.category_paths = []
-        HTMLParser.__init__(self)
-
-    def handle_starttag(self, tag, attrs):
-        attrs = dict((k, v) for k, v in attrs)
-        if tag == 'div' and 'class' in attrs:
-            if attrs['class'] == 'panel-body dslist2':
-                self.in_pannel = True
-        if tag == 'table' and 'class' in attrs:
-            if attrs['class'] == 'table table-bordered':
-                self.in_opinion_table = True
-        if tag == 'tr' and self.in_opinion_table:
-            self.in_table_row = True
-        if tag == 'a' and 'href' in attrs:
-            if self.in_table_row:
-                self.row_data.append(attrs['href'])
-            elif self.in_pannel:
-                self.category_paths.append(attrs['href'])
-    def handle_endtag(self, tag):
-        if tag == 'div':
-            self.in_pannel = False
-        if tag == 'table':
-            self.in_opinion_table = False;
-        if tag == 'tr' and self.row_data:
-            self.table_data.append(self.row_data)
-            self.row_data = []
-
-    def handle_data(self, data):
-        if self.in_table_row:
-            if data.strip():
-                self.row_data.append(data.strip())
-
-    @classmethod
-    def custom_parse(cls, html, return_variable):
-        parser = cls()
-        parser.feed(html)
-        parser.close()
-        return getattr(parser, return_variable)
+from datetime import datetime
+import lxml.html
 
 class Discovery:
     def __init__(self):
@@ -57,47 +14,55 @@ class Discovery:
         self.category_urls = []
         self.opinions = []
 
-    def fetch(self):
-        request = get(self.OPINIONS_MAIN_PAGE)
+    def run(self):
+        self.get_opinion_category_urls()
+        self.get_opinions_from_categories()
+
+    def get_opinion_category_urls(self):
+        request = get(self.OPINIONS_MAIN_PAGE, headers=settings.REQ_HEADER)
+
         if request.status_code == 200:
-  
-            # Fetch category urls
-            category_paths = SCOTUSParser.custom_parse(
-                request.text,
-                'category_paths',
-            )
-            for path in category_paths:
-                self.category_urls.append(self.OPINIONS_BASE + path)
+            html = lxml.html.fromstring(request.text)
+            search = "//div[@class='panel-body dslist2']/ul/li/a/@href"
+            for category in html.xpath(search):
+                self.category_urls.append(self.OPINIONS_BASE + category)
+            
+    def get_opinions_from_categories(self):
+        for category_url in self.category_urls:
+            category = category_url.split('/')[-2]
+           
+            request = get(category_url, headers=settings.REQ_HEADER)    
+            if request.status_code == 200:
+                html = lxml.html.fromstring(request.text)
+                search = "//table[@class='table table-bordered']/tr"
+                for row in html.xpath(search):
+                    opinion = []
+                    for cell in row.xpath('./td'):
+                        opinion.append(cell.text_content())
+                        for pdf_path in cell.xpath('./a/@href'):
+                            opinion.append(self.BASE + pdf_path)
+                            
+                    if opinion:
 
-            # Fetch opinion urls
-            for category in self.category_urls:
-                request = get(category)
-                if request.status_code == 200:
-                    table_data = SCOTUSParser.custom_parse(
-                        request.text,
-                        'table_data',
-                    )
+                        # Slip opinions have extra 'reporter' as first column
+                        # add blank first column to non slip opinions
+                        if len(opinion) == 6:
+                            opinion = [''] + opinion
 
-                    # Remove first row, it's the header fields
-                    del table_data[0]
-
-                    for row in table_data:
-               
-                        # Slip opinions have extra "reporter" column at the front
-                        # If not slip, add blank element to list at front
-                        if len(row) == 6:
-                            row = [''] + row
-
+                        # Standardize published date from American mm/dd/yy
+                        # or mm-dd-yy format
+                        opinion[1] = opinion[1].replace('-', '/')
+                        opinion[1] = datetime.strptime(opinion[1], "%m/%d/%y").strftime("%Y-%m-%d")                
                         self.opinions.append({
-                            'reporter': row[0],
-                            'published': row[1],
-                            'docket': row[2],
-                            'pdf_url': self.BASE + row[3],
-                            'name': row[4],
-                            'justice': row[5],
-                            'part': row[6],
+                            'category': category,
+                            'reporter': opinion[0],
+                            'published': opinion[1],
+                            'docket': opinion[2],
+                            'name': opinion[3],
+                            'pdf_url': opinion[4],
+                            'justice': opinion[5],
+                            'part': opinion[6],
                         })
 
-    def import_new_opinions(self):
-        for opinion in self.opinions:
-            print "OPINION:", opinion['pdf_url']
+    def scrape_urls_from_pdf(self):
+        pass
