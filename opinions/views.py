52a71174413ade0c87d703b824fbbce39b9df64c
orangeoval <anorangeoval@gmail.com>
2015-04-28 22:16:00 -0700

    fleshing out opinions views and template presentation

--- opinions/views.py
+++ opinions/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from opinions.models import Opinion
+from citations.models import Citation
 
 def index(request):
     template = 'opinions.html'
-    context = {'template': template};
+    opinions = Opinion.objects.all().order_by('-published')
+
+    for opinion in opinions:
+
+        # Get citation counts
+        opinion.citation_count = Citation.objects.filter(opinion=opinion.id).count()
+
+        # Get updated sate
+        if opinion.updated:
+            opinion.updated_date = Opinion.objects.filter(name=opinion.name).latest('published').published
+
+
+    context = {
+        'opinions': opinions,
+    }
+
     return render(request, template, context)
